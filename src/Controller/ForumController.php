0b3b8b38cdc35f76efffc87938222ae7fd9f0a93
angeliqueTourinov <angelique@tourinov.net>
2025-07-01 16:05:07 +0200

    route doc ok

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,38 +2,48 @@
 
 namespace App\Controller;
 
-use App\Service\FileUploader;
+use App\Entity\Board;
+use App\Entity\Category;
+use App\Entity\Topic;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ForumController extends AbstractController
+class ForumController extends AbstractController
 {
-    #[Route('/forum', name: 'app_forum')]
-    public function index(): Response
-    {
-        // return $this->render('forum/index.html.twig', [
-        //     'controller_name' => 'ForumController',
-        // ])
-        // ;
+#[Route('/forum', name: 'forum_index')]
+public function index(CategoryRepository $categoryRepository): Response
+{
+    $categories = $categoryRepository->findAll();
 
-        return $this->render('forum/index.html.twig', [
-            'topic' => 'Sujet fictif',
-            'messages' => [],
+    return $this->render('forum/index.html.twig', [
+        'categories' => $categories,
+    ]);
+    }
+
+    #[Route('/category/{id}', name: 'category')]
+    public function showCategory(Category $category): Response
+    {
+        return $this->render('forum/category.html.twig', [
+            'category' => $category,
         ]);
     }
 
-    public function new(Request $request, FileUploader $fileUploader)
+    #[Route('/board/{id}', name: 'board_show')]
+    public function showBoard(Board $board): Response
     {
-        $form = '$form';
+        return $this->render('forum/board.html.twig', [
+            'board' => $board,
+        ]);
+    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $file = $form->get('attachment')->getData();
-            if ($file) {
-                $fileName = $fileUploader->upload($file);
-                // stocker $fileName dans l'entité
-            }
-        }
+    #[Route('/topic/{id}', name: 'topic_show')]
+    public function showTopic(Topic $topic): Response
+    {
+        return $this->render('forum/topic.html.twig', [
+            'topic' => $topic,
+            'messages' => $topic->getMessages(), // si relation bien définie
+        ]);
     }
 }
--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Topic;
 use App\Repository\TopicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
